de84e2028a5e27e0942d351d84ae2b704eddc69f
Naing Linn Aung <naing@evantek.com>
2025-08-19 10:51:29 +0800

    Emphasize the api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL:'https://api.rawg.io/api',
     params:{
-        key:'9f83b11f8eb2422a94ee46ccbc3efea2'
+        key:import.meta.env.VITE_API_KEY
     }
 })
