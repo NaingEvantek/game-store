cc8db738254ea58561d9fc8583f49a6eef179c6f
Naing Linn Aung <naing@evantek.com>
2025-08-19 11:04:33 +0800

    Create a custom hook to fetch the games

---
+++ src/hooks/useGame.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+export interface Game {
+    id: number;
+    name: string;
+  }
+  
+export interface FetchGamesResponse {
+    count: number;
+    results: Game[];
+  }
+
+const useGames=()=>{
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState("");
+  
+    useEffect(() => {
+      const  controller = new AbortController(); // need to create if you want to control cancelled error.
+
+      apiClient
+        .get<FetchGamesResponse>("/games",{signal: controller.signal}) // {signal : ....} to catch the signal of api request
+        .then((res) => setGames(res.data.results))
+        .catch((err) => {
+            if(err instanceof CanceledError)return; // to remove cancelled error showing in error
+
+            setError(err.message);
+        });
+
+        return ()=>controller.abort(); // axios always call the api two times , first request need to cancelled to avoid double jobs.
+    }, []); // [] empty depedency
+
+    return {games,error};
+}
+
+export default useGames;
