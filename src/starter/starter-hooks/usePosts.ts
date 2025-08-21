c2c31458c436bbebdb0c82b9921dbc896c3e5136
Naing Linn Aung <naing@evantek.com>
2025-08-21 09:50:42 +0800

    Using Infinite Query in react-query

--- src/starter/starter-hooks/usePosts.ts
+++ src/starter/starter-hooks/usePosts.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 interface Post {
@@ -8,24 +8,46 @@ interface Post {
     userId: number;
   }
 
+
 interface PostQuery{
-    page:number;
-    pageSize:number;
+    // page:number;// need in useQuery , no need in infinitequery
+    pageSize:number; 
 }
-const usePosts=(query:PostQuery)=>useQuery<Post[],Error>({
-   // queryKey:userId?['users',userId,'posts']:['posts'],
-    queryKey:['posts',query],
-    queryFn:()=> axios
-        .get("https://jsonplaceholder.typicode.com/posts",{
-            params:{
-                _start:(query.page-1)*query.pageSize,
-                _limit:query.pageSize
-            }
-        })
-        .then((res) =>res.data),
-    staleTime:1*60*1000, //1m
-    keepPreviousData:true
-})
+
+// using useQuery
+// const usePosts=(query:PostQuery)=>useQuery<Post[],Error>({
+//    // queryKey:userId?['users',userId,'posts']:['posts'],
+//     queryKey:['posts',query],
+//     queryFn:()=> axios
+//         .get("https://jsonplaceholder.typicode.com/posts",{
+//             params:{
+//                 _start:(query.page-1)*query.pageSize,
+//                 _limit:query.pageSize
+//             }
+//         })
+//         .then((res) =>res.data),
+//     staleTime:1*60*1000, //1m
+//     keepPreviousData:true
+// })
+
+//using infinite query
+const usePosts=(query:PostQuery)=>useInfiniteQuery<Post[],Error>({
+    // queryKey:userId?['users',userId,'posts']:['posts'],
+     queryKey:['posts',query],
+     queryFn:({pageParam=1})=> axios
+         .get("https://jsonplaceholder.typicode.com/posts",{
+             params:{
+                 _start:(pageParam-1)*query.pageSize, 
+                 _limit:query.pageSize
+             }
+         })
+         .then((res) =>res.data),
+     staleTime:1*60*1000, //1m
+     keepPreviousData:true,
+     getNextPageParam:(lastPage,allPages)=>{ // only for infinite query
+       return lastPage.length>0? allPages.length+1:undefined;
+     }
+ })
 
 
 export default usePosts;
--- src/starter/starter-react-query/PostList.tsx
+++ src/starter/starter-react-query/PostList.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import usePosts from "../starter-hooks/usePosts";
+import React from "react";
 
 const PostList = () => {
-  const pageSize = 10;
-  const [page, setPage] = useState(1);
-  const { data, isLoading, error } = usePosts({ page, pageSize });
+  //use in useQuery , infinite query does not have pagination
+  // const pageSize = 10;
+  // const [page, setPage] = useState(1);
+  //const { data, isLoading, error } = usePosts({ page, pageSize });
 
+  //using infiniteQuery
+  const pageSize = 10;
+  const { data, isLoading, error, fetchNextPage, isFetchingNextPage } =
+    usePosts({ pageSize });
   if (isLoading) return <p>Loading...</p>;
 
   if (error) return <p>{error.message}</p>;
@@ -13,24 +19,29 @@ const PostList = () => {
   return (
     <>
       <ul className="list-group">
-        {data?.map((post) => (
-          <li key={post.id} className="list-group-item">
-            {post.title}
-          </li>
+        {data.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.map((post) => (
+              <li key={post.id} className="list-group-item">
+                {post.title}
+              </li>
+            ))}
+          </React.Fragment>
         ))}
       </ul>
-      <button
+      {/* <button
         disabled={page === 1}
         onClick={() => setPage(page - 1)}
         className="btn btn-primary my-3"
       >
         Previous
-      </button>
+      </button> //only for useQuery */}
       <button
         className="btn btn-primary my-3 ms-1"
-        onClick={() => setPage(page + 1)}
+        onClick={() => fetchNextPage()}
+        disabled={isFetchingNextPage}
       >
-        Next
+        {isFetchingNextPage ? "Loading..." : "Load More"}
       </button>
     </>
   );
