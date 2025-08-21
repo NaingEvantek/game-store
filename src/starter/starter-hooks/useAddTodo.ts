67a5fe16d3df27b7df1a9f549839963c317db41c
Naing Linn Aung <naing@evantek.com>
2025-08-21 12:29:08 +0800

    Creating a Custom Mutation Hook

---
+++ src/starter/starter-hooks/useAddTodo.ts
@@ -0,0 +1,43 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Todo } from "./useTodos";
+import axios from "axios";
+import { CACHE_KEY_TODOS } from "../starter-react-query/constants";
+
+interface AddTodoContext {
+    previousTodos: Todo[];
+  }
+  
+const useAddTodo=(onAdd:()=>void)=>{
+    const queryClient = useQueryClient();
+
+    return useMutation<Todo, Error, Todo, AddTodoContext>({
+      mutationFn: (todo: Todo) =>
+        axios
+          .post<Todo>("https://jsonplaceholder.typicode.com/posts", todo)
+          .then((res) => res.data),
+      onMutate: (newTodo: Todo) => {
+        const previousTodos = queryClient.getQueryData<Todo[]>(CACHE_KEY_TODOS) || [];
+  
+        queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos = []) => [
+          newTodo,
+          ...todos,
+        ]);
+  
+        onAdd();
+
+        return { previousTodos };
+      },
+      onSuccess: (saveTodo, newTodo) => {
+        queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos) =>
+          todos?.map((todo) => (todo === newTodo ? saveTodo : todo))
+        );
+      },
+      onError: (error, newTodo, context) => {
+        if (!context) return;
+  
+        queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, context.previousTodos);
+      },
+    });
+}
+
+export default useAddTodo;
