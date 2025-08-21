8d125e1a06457f24440f9a8ac8b94d1bc5235b36
Naing Linn Aung <naing@evantek.com>
2025-08-21 10:11:12 +0800

    Build a Mutation Data

--- src/starter/StarterApp.tsx
+++ src/starter/StarterApp.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import TodoList from "./starter-react-query/TodoList";
 import PostList from "./starter-react-query/PostList";
+import TodoForm from "./starter-react-query/TodoForm";
 
 const StarterApp = () => {
-  return <PostList />;
+  return (
+    <>
+      <TodoForm />
+      <TodoList />
+    </>
+  );
 };
 
 export default StarterApp;
