cfe51a8b2eff6722120e3579d8348974d19da220
Naing Linn Aung <naing@evantek.com>
2025-08-25 09:52:05 +0800

    Fetch game details

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,16 +4,16 @@ import GameDetailPage from "./pages/GameDetailPage";
 import HomePage from "./pages/HomePage";
 import Layout from "./pages/Layout";
 
-const router=createBrowserRouter([
-    {
-        path:'/',
-        element:<Layout/>,
-        errorElement:<ErrorPage/>,
-        children:[
-            {index:true,element:<HomePage/>},
-            {path:'games/:id',element:<GameDetailPage/>}
-        ]
-    }
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "games/:slug", element: <GameDetailPage /> },
+    ],
+  },
 ]);
 
 export default router;
